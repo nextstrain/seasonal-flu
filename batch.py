ddcb871a75fc4371584cbf7fdb95718440506b7f
Trevor Bedford <trevor@bedford.io>
2019-01-29 17:31:53 -0800

    Rewrite batch.py to be compatible with both live builds and who builds
    The call logic is slightly different between live and who. For example, live builds should not be run with '-s Snakemake_WHO'. The existing batch.py script had stripped out the ability to run live builds. This is restored and a couple additional command line flags added.

--- batch.py
+++ batch.py
@@ -12,8 +12,10 @@ import os
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Run flu builds on aws', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--system', type = str, default = 'local', help='where to run, local or batch')
+    parser.add_argument('-v', '--version', type = str, default = 'live', help='version to run, live or who or both')
     parser.add_argument('-l', '--lineages', nargs='+', type = str,  help ="flu lineages to include", default=['h3n2', 'h1n1pdm', 'vic', 'yam'])
-    parser.add_argument('-r', '--resolutions', nargs='+', type = str,  help ="flu resolutions to include", default=['2y', '6y'])
+    parser.add_argument('-r', '--resolutions', nargs='+', type = str,  help ="flu resolutions to include", default=['2y', '3y', '6y' , '12y'])
     parser.add_argument('-s', '--segments', nargs='+', type = str, help ="flu segments to include", default=['ha', 'na'])
     parser.add_argument('-c', '--centers', nargs='+', type = str, help ="who collaborating centers to include", default=['cdc', 'who', 'niid', 'vidrl', 'crick'])
     parser.add_argument('-p', '--passages', nargs='+', type = str, help ="passages to include", default=['cell', 'egg'])
@@ -26,45 +28,52 @@ if __name__ == '__main__':
         rmc = 'rm -rf logs/*'
         subprocess.call(rmc, shell=True)
 
-    for center in params.centers:
+    if params.version == 'live' or params.version == 'both':
         for lineage in params.lineages:
             for resolution in params.resolutions:
-                for passage in params.passages:
-                    if lineage == "h3n2":
-                        assays = params.assays
-                        for assay in assays:
-                            call = ['nextstrain', 'build', '--aws-batch', '.', '-s Snakefile_WHO', '-j 2']
+                if params.system == 'local':
+                    call = ['nextstrain', 'build', '.', '-j', '2']
+                elif params.system == 'batch':
+                    call = ['nextstrain', 'build', '--aws-batch', '.', '-j', '2']
+                targets = []
+                for segment in params.segments:
+                    targets.append('auspice/flu_seasonal_%s_%s_%s_tree.json'%(lineage, segment, resolution))
+                    targets.append('auspice/flu_seasonal_%s_%s_%s_meta.json'%(lineage, segment, resolution))
+                    targets.append('auspice/flu_seasonal_%s_%s_%s_tip-frequencies.json'%(lineage, segment, resolution))
+                call.extend(targets)
+                print(' '.join(call))
+                log = open('logs/%s_%s.txt'%(lineage, resolution), 'w')
+                if params.system == 'local':
+                    pro = subprocess.call(call)
+                if params.system == 'batch':
+                    pro = subprocess.Popen(call, stdout=log, stderr=log)
+
+    if params.version == 'who' or params.version == 'both':
+        for center in params.centers:
+            for lineage in params.lineages:
+                resolutions = [r for r in params.resolutions if r == '2y' or r == '6y']                
+                for resolution in resolutions:
+                    for passage in params.passages:
+                        for assay in params.assays:
+                            segment = 'ha'
+                            if lineage != 'h3n2' and assay == 'fra':
+                                break
+                            if params.system == 'local':
+                                call = ['nextstrain', 'build', '.', '-s', 'Snakefile_WHO', '-j', '1']
+                            elif params.system == 'batch':
+                                call = ['nextstrain', 'build', '--aws-batch', '.', '-s', 'Snakefile_WHO', '-j', '1']
                             targets = []
-                            for segment in params.segments:
-                                # auspice-who/flu_cdc_h3n2_ha_2y_cell_hi_tree.json, auspice-who/flu_cdc_h3n2_ha_2y_egg_hi_tree.json, auspice-who/flu_cdc_h3n2_ha_2y_cell_fra_tree.json, auspice-who/flu_cdc_h3n2_ha_2y_egg_fra_tree.json, auspice-who/flu_cdc_h3n2_ha_6y_cell_hi_tree.json, auspice-who/flu_cdc_h3n2_ha_6y_egg_hi_tree.json, auspice-who/flu_cdc_h3n2_ha_6y_cell_fra_tree.json, auspice-who/flu_cdc_h3n2_ha_6y_egg_fra_tree.json, auspice-who/flu_cdc_h3n2_na_2y_cell_hi_tree.json
-                                targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_tree.json'%(center, lineage, segment, resolution, passage, assay))
-                                targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_meta.json'%(center, lineage, segment, resolution, passage, assay))
-                                targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_entropy.json'%(center, lineage, segment, resolution, passage, assay))
-                                # targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_sequences.json'%(center, lineage, segment, resolution, passage, assay))
-                                targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_titer_subs_model.json'%(center, lineage, segment, resolution, passage, assay))
-                                targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_titer_tree_model.json'%(center, lineage, segment, resolution, passage, assay))
-                                targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_titers.json'%(center, lineage, segment, resolution, passage, assay))
+                            targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_tree.json'%(center, lineage, segment, resolution, passage, assay))
+                            targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_meta.json'%(center, lineage, segment, resolution, passage, assay))
+                            targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_entropy.json'%(center, lineage, segment, resolution, passage, assay))
+                            targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_titer-subs-model.json'%(center, lineage, segment, resolution, passage, assay))
+                            targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_titer-tree-model.json'%(center, lineage, segment, resolution, passage, assay))
+                            targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_titers.json'%(center, lineage, segment, resolution, passage, assay))
 
                             call.extend(targets)
                             print(' '.join(call))
                             log = open('logs/%s_%s.txt'%(lineage, resolution), 'w')
-                            # run in background with subprocess.Popen
-                            pro = subprocess.Popen(call, stdout=log, stderr=log)
-                    else:
-                        assay = 'hi'
-                        call = ['nextstrain', 'build', '--aws-batch', '.', '-s Snakefile_WHO', '-j 2']
-                        targets = []
-                        for segment in params.segments:
-                            targets.append('auspice/flu_%s_%s_%s_%s_%s_%s_tree.json'%(center, lineage, segment, resolution, passage, assay))
-                            targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_meta.json'%(center, lineage, segment, resolution, passage, assay))
-                            targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_entropy.json'%(center, lineage, segment, resolution, passage, assay))
-                            # targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_sequences.json'%(center, lineage, segment, resolution, passage, assay))
-                            targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_titer_subs_model.json'%(center, lineage, segment, resolution, passage, assay))
-                            targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_titer_tree_model.json'%(center, lineage, segment, resolution, passage, assay))
-                            targets.append('auspice-who/flu_%s_%s_%s_%s_%s_%s_titers.json'%(center, lineage, segment, resolution, passage, assay))
-
-                        call.extend(targets)
-                        print(' '.join(call))
-                        log = open('logs/%s_%s.txt'%(lineage, resolution), 'w')
-                        # run in background with subprocess.Popen
-                        pro = subprocess.Popen(call, stdout=log, stderr=log)
+                            if params.system == 'local':
+                                pro = subprocess.call(call)
+                            if params.system == 'batch':
+                                pro = subprocess.Popen(call, stdout=log, stderr=log)
