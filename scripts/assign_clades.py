2ee08cc0e0a4695476b1e28669910a1d75db06c8
Richard Neher <richard.neher@unibas.ch>
2019-07-23 16:41:28 +0200

    first shot at automatic clade assignment

---
+++ scripts/assign_clades.py
@@ -0,0 +1,149 @@
+import argparse
+from Bio import Phylo
+import numpy as np
+from treetime.utils import numeric_date
+from augur.utils import read_metadata, get_numerical_dates, read_node_data, write_json
+from select_strains import parse_metadata
+
+nameing_alphabets = [list(map(str,range(10))),
+                     list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+                     list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.lower())]
+
+separator = '.'
+frequency_threshold = 0.1
+distance_threshold = 3
+
+def next_clade(previous, alphabet):
+    max_digits = len(previous)+1
+    n = len(alphabet)
+    digit_representation = [alphabet.index(x) for x in previous]
+    digit_representation.reverse()
+    c = int(np.sum([b*n**i for i,b in enumerate(digit_representation)]))+1
+    new_digits = [alphabet[(c//(n**i))%n] for i in range(max_digits) if c//(n**i)]
+    new_digits.reverse()
+    return ''.join(new_digits)
+
+
+def generate_clade_name(node, existing_names):
+    super_clade = node.clade
+    sister_clades = sorted([clade.lstrip(super_clade+separator) for clade in existing_names
+                     if clade.startswith(super_clade)])
+
+    compatible_alphabets = [a for a in nameing_alphabets
+                            if all([(x in a) or x==separator for x in super_clade])]
+
+    if len(sister_clades):
+        new_clade = super_clade + separator + next_clade(sister_clades[-1], compatible_alphabets[0])
+    else:
+        new_clade = super_clade + separator + compatible_alphabets[0][0]
+
+    return new_clade
+
+
+def assign_new_clade_name(node, new_name):
+    for n in node.find_clades(order='preorder'):
+        if n==node:
+            node.clade = new_name
+            node.distance_from_last_clade = 0
+        else:
+            n.clade = n.up.clade
+            n.distance_from_last_clade = n.up.distance_from_last_clade + len(n.mutations)
+
+
+def name_new_clades(tree, tree_frequency_index):
+    assert hasattr(tree.root, 'clade')
+    existing_clades = set()
+    for n in tree.find_clades(order='preorder'):
+        if n.up is None:
+            existing_clades.add(n.clade)
+            n.distance_from_last_clade = 0
+            continue
+
+        if not hasattr(n, "clade"):
+            n.clade = n.up.clade
+
+        if n.clade==n.up.clade:
+            n.distance_from_last_clade = n.up.distance_from_last_clade + len(n.mutations)
+        else:
+            existing_clades.add(n.clade)
+            n.distance_from_last_clade = 0
+
+    for n in tree.find_clades(order='preorder'):
+        if n.freq[tree_frequency_index]>frequency_threshold and \
+           n.distance_from_last_clade>distance_threshold:
+            new_name = generate_clade_name(n, existing_clades)
+            assign_new_clade_name(n, new_name)
+            existing_clades.add(new_name)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Assign clade names",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument('--tree', type=str, required=True,
+                        help="newick file with the tree")
+    parser.add_argument('--metadata', nargs='+', help="file with metadata associated with viral sequences, one for each segment")
+    parser.add_argument('--mutations', nargs='+', help='JSON(s) containing ancestral and tip nucleotide and/or amino-acid mutations ')
+    parser.add_argument('--date-format', type=str, default="%Y-%m-%d", help="date format")
+    parser.add_argument('--output', help="name of the file to write selected strains to")
+
+
+    args = parser.parse_args()
+
+    T = Phylo.read(args.tree, 'newick')
+    # read in meta data, parse numeric dates
+    metadata = parse_metadata(['ha'], args.metadata, date_format=args.date_format)['ha']
+    node_data = read_node_data(args.mutations, args.tree)
+
+    all_dates = []
+    for n in T.get_terminals():
+        if n.name in metadata:
+            n.num_date = metadata[n.name]['num_date']
+            all_dates.append(n.num_date)
+        else:
+            n.num_date = None
+
+
+    all_dates.sort()
+    date_range = (all_dates[int(0.05*len(all_dates))], all_dates[int(0.95*len(all_dates))])
+    dt = 0.5
+    date_bins = np.arange(date_range[0], date_range[1]+dt,dt)
+
+    for n in T.find_clades(order='postorder'):
+        if n.name in node_data["nodes"]:
+            n.mutations = node_data["nodes"][n.name]['muts']
+        if n.is_terminal():
+            n.bin_count = np.zeros_like(date_bins)
+            n.bin_count[date_bins.searchsorted(n.num_date)] += 1
+        else:
+            n.bin_count = np.sum([c.bin_count for c in n], axis=0)
+
+    T.root.up = None
+    for n in T.find_clades(order='preorder'):
+        n.freq = n.bin_count/T.root.bin_count
+        if not n.is_terminal():
+            for c in n:
+                c.up = n
+
+    T.root.clade = '0'
+    for i,threshold in enumerate(date_bins):
+        name_new_clades(T, i)
+
+    for n in T.get_terminals():
+        print(n.clade)
+
+    clades = {}
+    for n in T.find_clades():
+        if n.up:
+            if n.up.clade!=n.clade:
+                clades[n.name] = {'clade_annotation': c.clade, 'clade_membership':c.clade}
+            else:
+                clades[n.name] = {'clade_membership':c.clade}
+        else:
+            clades[n.name] = {'clade_annotation': c.clade, 'clade_membership':c.clade}
+
+    write_json({'nodes': clades}, args.output)
+
