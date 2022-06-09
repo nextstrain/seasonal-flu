165ddc840b7566d5c4b143a7ec3e86b9684aaa23
Richard Neher <richard.neher@unibas.ch>
2022-06-09 12:31:32 +0200

    fix submit

--- profiles/scicore/submit.sh
+++ profiles/scicore/submit.sh
@@ -6,7 +6,7 @@
 # activate conda environment
 source ~/miniconda3/etc/profile.d/conda.sh
 export PATH="/scicore/home/neher/GROUP/bin:/scicore/home/neher/neher/julia-1.7.2/bin:/scicore/home/neher/neher/.julia/bin:"$PATH
-conda activate treeknit
+conda activate nextstrain
 
 #Test
 export AUGUR_MINIFY_JSON=1
