68fa2d99d6d77cd6a6e6b72df8f345d371d4551c
Jover <joverlee521@gmail.com>
2022-09-22 16:53:51 -0700

    scripts/prune_reference: allow empty reference
    Print a warning if reference is empty, but still copy the input tree
    to the output tree. This is expected to happen for any resolution that
    is not 2y or 6m in our live builds.

--- scripts/prune_reference.py
+++ scripts/prune_reference.py
@@ -4,6 +4,8 @@ Prunes a reference strain from the provided tree.
 """
 import argparse
 from Bio import Phylo
+import shutil
+import sys
 
 
 if __name__ == '__main__':
@@ -12,15 +14,20 @@ if __name__ == '__main__':
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument("--tree", help="Newick tree to prune")
-    parser.add_argument("--reference", help="Name of the reference strain to prune")
+    parser.add_argument("--reference", nargs="?", help="Name of the reference strain to prune")
     parser.add_argument("--output", help="Output Newick tree file")
 
     args = parser.parse_args()
 
-    T = Phylo.read(args.tree, "newick")
-    references = [ c for c in T.find_clades() if str(c.name) == args.reference ]
-    if references:
-        T.prune(references[0])
+    # If reference is not provided, then just copy the input to output without modifications
+    if not args.reference:
+        print("WARNING: No reference was provided, copying input tree to output tree", file=sys.stdout)
+        shutil.copy(args.tree, args.output)
+    else:
+        T = Phylo.read(args.tree, "newick")
+        references = [ c for c in T.find_clades() if str(c.name) == args.reference ]
+        if references:
+            T.prune(references[0])
 
-    Phylo.write(T, args.output, "newick")
+        Phylo.write(T, args.output, "newick")
 
