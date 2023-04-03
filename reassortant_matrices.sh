c1f14aad3335aa9f3c80f1f2218cd56274bd40fa
John Huddleston <huddlej@gmail.com>
2023-04-03 10:42:24 -0700

    Remove old script for titer matrices plots

--- reassortant_matrices.sh
+++
@@ -1,5 +0,0 @@
-
-python scripts/plot_titer_matrices.py --titers auspice-who/flu_cdc_h3n2_ha_2y_egg_hi_titers.json --clades results/clades_cdc_h3n2_ha_2y_egg_hi.json --metadata results/metadata_h3n2_ha.tsv --reassortants "F16-601511" "F17-611291" "F17-E2840" --output figures/titer_matrix_cdc_h3n2_ha_2y_egg_hi_reassortants.pdf
-
-
-python scripts/plot_titer_matrices.py --titers auspice-who/flu_cdc_h3n2_ha_2y_egg_fra_titers.json --clades results/clades_cdc_h3n2_ha_2y_egg_fra.json --metadata results/metadata_h3n2_ha.tsv --reassortants "F16-601511" "F17-611291" "F17-E2840" --output figures/titer_matrix_cdc_h3n2_ha_2y_egg_fra_reassortants.pdf
