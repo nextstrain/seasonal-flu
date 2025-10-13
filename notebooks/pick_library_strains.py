ef7e5af42a333353f70c3cfd883974b73e3ffec8
John Huddleston <huddlej@gmail.com>
2025-10-13 16:08:58 -0700

    Refine notebook for experimental library design
    Updated build config for experimental library design to select strains
    circulating since May 1 and updated the corresponding notebook to
    include parameters for minimum sequences and minimum date and to analyze
    H1 and H3 datasets in the same notebook instance.

--- notebooks/pick_library_strains.py
+++ notebooks/pick_library_strains.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.14.17"
+__generated_with = "0.13.15"
 app = marimo.App(width="medium")
 
 
@@ -13,115 +13,180 @@ def _():
     return json, mo, pd
 
 
-@app.cell
-def _(mo):
-    lineage_picker = mo.ui.radio(options=["h1n1pdm", "h3n2"])
-    lineage_picker
-    return (lineage_picker,)
+@app.cell(hide_code=True)
+def _(
+    haplotypes_by_lineage,
+    min_date,
+    min_sequences,
+    mo,
+    nonsingleton_haplotypes_by_lineage,
+):
+    mo.md(
+        rf"""
+    ## Setup
 
+    Selected all GISAID isolates with:
 
-@app.cell
-def _(lineage_picker):
-    lineage = lineage_picker.value
-    return (lineage,)
+      - minimum collection date of {min_date}
+      - an HA record for the isolate
+      - a “good” overall QC status by Nextclade alignment
 
+    Excluded records with:
 
-@app.cell
-def _(lineage):
-    if lineage == "h1n1pdm":
-        distance_map_path = "config/distance_maps/h1n1pdm/ha/canton.json"
-    elif lineage == "h3n2":
-        distance_map_path = "config/distance_maps/h3n2/ha/wolf.json"
-    return (distance_map_path,)
+      - ambiguous month in collection date (e.g., 2025-XX-XX)
+      - excluded records with egg passaging
 
+    ## Summary
 
-@app.cell
-def _(distance_map_path, json):
-    with open(distance_map_path, "r", encoding="utf-8") as _fh:
-        distance_map = json.load(_fh)
+    For H1N1pdm, found {haplotypes_by_lineage['h1n1pdm']} distinct haplotypes including {nonsingleton_haplotypes_by_lineage['h1n1pdm']} with at least {min_sequences} sequences.
 
-    epitope_sites = set(distance_map["map"]["HA1"].keys())
-    return (epitope_sites,)
-
-
-@app.cell
-def _(json, lineage):
-    rbs_sites = set()
-    if lineage == "h3n2":
-        rbs_distance_map_path = "config/distance_maps/h3n2/ha/koel.json"
-        with open(rbs_distance_map_path, "r", encoding="utf-8") as _fh:
-            rbs_distance_map = json.load(_fh)
-
-        rbs_sites = set(rbs_distance_map["map"]["HA1"].keys())
-    return (rbs_sites,)
-
-
-@app.cell
-def _(epitope_sites, lineage, pd, rbs_sites):
-    df = pd.read_csv(
-        f"builds/{lineage}/metadata.tsv",
-        sep="\t",
-        dtype="str",
-        na_filter=False,
+    For H3N2, found {haplotypes_by_lineage['h3n2']} distinct haplotypes including {nonsingleton_haplotypes_by_lineage['h3n2']} with at least {min_sequences} sequences.
+    """
     )
-
-    df["founder_ha1_substitutions"] = df["founderMuts['subclade'].aaSubstitutions"].apply(
-        lambda subs: ",".join([
-            sub.replace("HA1:", "")
-            for sub in subs.split(",")
-            if sub.startswith("HA1:")
-        ])
-    )
-
-    df["derived_haplotype"] = (df["subclade"] + ":" + df["founder_ha1_substitutions"]).str.rstrip(":")
-
-    df["epitope_mutations_from_subclade"] = df["founder_ha1_substitutions"].apply(
-        lambda subs: sum(
-            sub[1:-1] in epitope_sites
-            for sub in subs.split(",")
-        )
-    )
-
-    df["rbs_mutations_from_subclade"] = df["founder_ha1_substitutions"].apply(
-        lambda subs: sum(
-            sub[1:-1] in rbs_sites
-            for sub in subs.split(",")
-        )
-    )
-
-    df["glycosylation_count"] = df["glycosylation"].apply(
-        lambda glyc: len([site for site in glyc.split(";") if site.startswith("HA1:")])
-    )
-    return (df,)
-
-
-@app.cell
-def _(df):
-    distinct_haplotypes = (df["derived_haplotype"].value_counts() > 0).sum()
-    return (distinct_haplotypes,)
-
-
-@app.cell
-def _(df):
-    non_singleton_distinct_haplotypes = (df["derived_haplotype"].value_counts() > 1).sum()
-    return (non_singleton_distinct_haplotypes,)
-
-
-@app.cell
-def _(distinct_haplotypes, lineage, mo, non_singleton_distinct_haplotypes):
-    mo.md(rf"""Found {distinct_haplotypes} distinct haplotypes for {lineage} including {non_singleton_distinct_haplotypes} with more than 1 sequence.""")
     return
 
 
 @app.cell
-def _(df):
-    strain_to_accession = dict(df.loc[:, ["strain", "gisaid_epi_isl"]].values)
-    return (strain_to_accession,)
+def _():
+    min_date = "2025-09-01"
+    return (min_date,)
 
 
 @app.cell
-def _(df, strain_to_accession):
-    summary = df.groupby("derived_haplotype").aggregate(
+def _(mo):
+    min_sequences_picker = mo.ui.number(
+        start=1,
+        stop=20,
+        value=2,
+        label="min sequences",
+    )
+    min_sequences_picker
+    return (min_sequences_picker,)
+
+
+@app.cell
+def _(min_sequences_picker):
+    min_sequences = min_sequences_picker.value
+    return (min_sequences,)
+
+
+@app.cell
+def _():
+    paths_by_lineage = {
+        "h1n1pdm": {
+            "distance_map": "config/distance_maps/h1n1pdm/ha/canton.json",
+            "metadata": "builds/h1n1pdm/metadata.tsv",
+            "output": "h1n1pdm_haplotypes.tsv",
+        },
+        "h3n2": {
+            "distance_map": "config/distance_maps/h3n2/ha/wolf.json",
+            "rbs_distance_map": "config/distance_maps/h3n2/ha/koel.json",
+            "metadata": "builds/h3n2/metadata.tsv",
+            "output": "h3n2_haplotypes.tsv",
+        },
+    }
+    return (paths_by_lineage,)
+
+
+@app.cell
+def _(json):
+    def load_distance_map(distance_map_path):
+        with open(distance_map_path, "r", encoding="utf-8") as fh:
+            distance_map = json.load(fh)
+
+        return set(distance_map["map"]["HA1"].keys())
+    return (load_distance_map,)
+
+
+@app.cell
+def _(pd):
+    def load_metadata(metadata_path, min_date=None, epitope_sites=None, rbs_sites=None):
+        if epitope_sites is None:
+            epitope_sites = set()
+
+        if rbs_sites is None:
+            rbs_sites = set()
+
+        df = pd.read_csv(
+            metadata_path,
+            sep="\t",
+            dtype="str",
+            na_filter=False,
+        )
+
+        if min_date:
+            df = df[df["date"] >= min_date].copy()
+
+        df["founder_ha1_substitutions"] = df["founderMuts['subclade'].aaSubstitutions"].apply(
+            lambda subs: ",".join([
+                sub.replace("HA1:", "")
+                for sub in subs.split(",")
+                if sub.startswith("HA1:")
+            ])
+        )
+
+        df["derived_haplotype"] = (df["subclade"] + ":" + df["founder_ha1_substitutions"]).str.rstrip(":")
+
+        df["epitope_mutations_from_subclade"] = df["founder_ha1_substitutions"].apply(
+            lambda subs: sum(
+                sub[1:-1] in epitope_sites
+                for sub in subs.split(",")
+            )
+        )
+
+        df["rbs_mutations_from_subclade"] = df["founder_ha1_substitutions"].apply(
+            lambda subs: sum(
+                sub[1:-1] in rbs_sites
+                for sub in subs.split(",")
+            )
+        )
+
+        df["glycosylation_count"] = df["glycosylation"].apply(
+            lambda glyc: len([site for site in glyc.split(";") if site.startswith("HA1:")])
+        )
+
+        return df
+    return (load_metadata,)
+
+
+@app.cell
+def _(
+    load_distance_map,
+    load_metadata,
+    min_date,
+    min_sequences,
+    paths_by_lineage,
+):
+    data_frame_by_lineage = {}
+    haplotypes_by_lineage = {}
+    nonsingleton_haplotypes_by_lineage = {}
+
+    for lineage, lineage_paths in paths_by_lineage.items():
+        epitope_sites = load_distance_map(lineage_paths["distance_map"])
+        rbs_sites = load_distance_map(lineage_paths["rbs_distance_map"]) if "rbs_distance_map" in lineage_paths else None
+
+        data_frame_by_lineage[lineage] = load_metadata(
+            lineage_paths["metadata"],
+            min_date,
+            epitope_sites,
+            rbs_sites,
+        )
+
+        haplotypes_by_lineage[lineage] = (data_frame_by_lineage[lineage]["derived_haplotype"].value_counts() > 0).sum()
+        nonsingleton_haplotypes_by_lineage[lineage] = (data_frame_by_lineage[lineage]["derived_haplotype"].value_counts() >= min_sequences).sum()
+    return (
+        data_frame_by_lineage,
+        haplotypes_by_lineage,
+        lineage_paths,
+        nonsingleton_haplotypes_by_lineage,
+    )
+
+
+@app.function
+def summarize_haplotypes(data_frame):
+    strain_to_accession = dict(data_frame.loc[:, ["strain", "gisaid_epi_isl"]].values)
+
+    summary = data_frame.groupby("derived_haplotype").aggregate(
         count=("derived_haplotype", "count"),
         mean_epitope_mutations=("epitope_mutations_from_subclade", "mean"),
         mean_rbs_mutations=("rbs_mutations_from_subclade", "mean"),
@@ -136,21 +201,34 @@ def _(df, strain_to_accession):
     summary["representative_strain_accession"] = summary["representative_strain"].map(
         strain_to_accession
     )
-    return (summary,)
+
+    return summary
 
 
 @app.cell
-def _(summary):
-    summary
+def _(data_frame_by_lineage, lineage_paths, paths_by_lineage):
+    summary_by_lineage = {}
+    for _lineage, _lineage_paths in paths_by_lineage.items():    
+        summary_by_lineage[_lineage] = summarize_haplotypes(
+            data_frame_by_lineage[_lineage],
+        )
+
+        summary_by_lineage[_lineage].to_csv(
+            lineage_paths["output"],
+            sep="\t",
+        )
+    return (summary_by_lineage,)
+
+
+@app.cell
+def _(summary_by_lineage):
+    summary_by_lineage["h1n1pdm"]
     return
 
 
 @app.cell
-def _(lineage, summary):
-    summary.to_csv(
-        f"{lineage}_haplotypes.tsv",
-        sep="\t",
-    )
+def _(summary_by_lineage):
+    summary_by_lineage["h3n2"]
     return
 
 
