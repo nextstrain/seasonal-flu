1a1df0f037995372a35217b7f711dc7f40f27cec
John Huddleston <huddlej@gmail.com>
2025-10-22 13:42:48 -0700

    WIP: Label polyphyletic branches

--- scripts/table_to_node_data.py
+++ scripts/table_to_node_data.py
@@ -5,7 +5,7 @@ import sys
 
 import pandas as pd
 
-from augur.utils import read_tree, write_json
+from augur.utils import annotate_parents_for_tree, read_tree, write_json
 
 
 if __name__ == "__main__":
@@ -17,6 +17,7 @@ if __name__ == "__main__":
     parser.add_argument("--node-name", default="nodes", help="name of the node data attribute in the JSON output")
     parser.add_argument("--branch-labels", nargs="+", help="optional map of column names to branch labels. You must supply this for each column you would like to label. By default the branch label key the same as the column name, but you may customise this via the COLUMN=LABEL syntax.")
     parser.add_argument("--column-to-node-attribute", nargs="+", help="optional map of column names to node attributes in the node data JSON in the format of COLUMN=ATTRIBUTE")
+    parser.add_argument("--error-on-polyphyletic-labels", action="store_true", help="exit with an error if the script finds any polyphyletic branch labels")
     parser.add_argument("--output", required=True, help="node data JSON file")
 
     args = parser.parse_args()
@@ -47,6 +48,7 @@ if __name__ == "__main__":
     }
 
     # Optionally annotate branch labels for internal nodes, if a tree is given.
+    found_polyphyletic_labels = False
     if args.branch_labels:
         if not args.tree:
             print(
@@ -56,7 +58,7 @@ if __name__ == "__main__":
             sys.exit(1)
 
         # Load the tree.
-        tree = read_tree(args.tree)
+        tree = annotate_parents_for_tree(read_tree(args.tree))
 
         # Parse branch columns and labels.
         branch_label_by_column = {}
@@ -74,6 +76,7 @@ if __name__ == "__main__":
         for column, label in branch_label_by_column.items():
             # Get distinct values for this column.
             branch_values = set(table[column].drop_duplicates().values)
+            labeled_branch_values = set()
 
             # Map each node to its value.
             value_by_node = dict(table[column].reset_index().values)
@@ -82,13 +85,45 @@ if __name__ == "__main__":
             # each distinct value.
             for node in tree.find_clades():
                 node_value = value_by_node.get(node.name)
-                if node_value in branch_values:
+                parent_node_value = value_by_node.get(getattr(node.parent, "name", None))
+
+                # Check whether the current node value is among the values we
+                # want to label.
+                if node_value in branch_values and node_value != parent_node_value:
+                    # Check whether the current value has already been assigned
+                    # to a branch. If it hasn't, assign the label to the branch.
+                    # If it has been assigned and the current node represents
+                    # another clade with the value, let the user know that the
+                    # value could not be assigned monophyletically.
                     if node.name not in branches:
                         branches[node.name] = {"labels": {}}
 
-                    branches[node.name]["labels"][label] = node_value
-                    branch_values.discard(node_value)
+                    if node_value not in labeled_branch_values:
+                        branches[node.name]["labels"][label] = node_value
+                        labeled_branch_values.add(node_value)
+                    else:
+                        print(
+                            f"WARNING: The value {node_value} for the column {column} is not monophyletic ({node.name})",
+                            file=sys.stderr,
+                        )
+
+                    branches[node.name]["labels"][label + "_by_node"] = node_value + ":" + node.name
+                    # if node_value not in labeled_branch_values:
+                    #     if node.name not in branches:
+                    #         branches[node.name] = {"labels": {}}
+
+                    #     branches[node.name]["labels"][label] = node_value
+                    #     labeled_branch_values.add(node_value)
+                    # elif node_value != parent_node_value:
+                    #     found_polyphyletic_labels = True
+                    #     print(
+                    #         f"WARNING: The value {node_value} for the column {column} is not monophyletic ({node.name})",
+                    #         file=sys.stderr,
+                    #     )
 
         node_data["branches"] = branches
+
+    if found_polyphyletic_labels and args.error_on_polyphyletic_labels:
+        sys.exit(1)
 
     write_json(node_data, args.output)
